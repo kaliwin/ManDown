2bcc57b49f8efb366930f095f017a04add8f59d0
cyvk <54166512+kaliwin@users.noreply.github.com>
2023-07-12 17:15:03 +0800

    Update jsrpc.js

--- jsrpc.js
+++ jsrpc.js
@@ -11,12 +11,22 @@
         
         var client = new SekiroClientTest("ws://127.0.0.1:5612/business-demo/register?group=test&clientId=" + guid());
         //注册一个行动监听
-        client.registerAction("vim", function (request, resolve, reject) {
+        client.registerAction("encrypt", function (request, resolve, reject) {
             try {
-            
-                var user = request['name']
+        
+                var data = request['data']
              //   var cyvk = window._cyvk(user);
-                resolve(window._f(user))
+                resolve(window._d(data))
+            } catch (e) {
+                reject("error: " + e);
+            }
+        });
+        client.registerAction("Decrypt", function (request, resolve, reject) {
+            try {
+        
+                var data = request['data']
+             //   var cyvk = window._cyvk(user);
+                resolve(window._d(data))
             } catch (e) {
                 reject("error: " + e);
             }
@@ -92,64 +102,3 @@
                 e.connect()
             }, 2e3)
         })​
-
-由于部分人无法领悟到他的威力, 特此讲解让你们感受一下他的威力
-
-
-
-资源:
-
-sekiro : http://sekiro.iinti.cn/sekiro-doc/  ( 官方文档 jsRPC )
-
-sekiro下载地址: https://oss.iinti.cn/sekiro/sekiro-demo 
-
-MorePossibility: https://github.com/kaliwin/MorePossibility 
-
-go-Api : https://github.com/kaliwin/MorePossibility-GO  (以后api都会放着直接引用即可)
-    }, SekiroClientTest.prototype.handleSekiroRequest = function (e) {
-        console.log("receive sekiro request: " + e);
-        var o = JSON.parse(e), t = o.__sekiro_seq__;
-        if (o.action) {
-            var n = o.action;
-            if (this.handlers[n]) {
-                var s = this.handlers[n], i = this;
-                try {
-                    s(o, function (e) {
-                        try {
-                            i.sendSuccess(t, e)
-                        } catch (e) {
-                            i.sendFailed(t, "e:" + e)
-                        }
-                    }, function (e) {
-                        i.sendFailed(t, e)
-                    })
-                } catch (e) {
-                    console.log("error: " + e), i.sendFailed(t, ":" + e)
-                }
-            } else this.sendFailed(t, "no action handler: " + n + " defined")
-        } else this.sendFailed(t, "need request param {action}")
-    }, SekiroClientTest.prototype.sendSuccess = function (e, o) {
-        var t;
-        if ("string" == typeof o) try {
-            t = JSON.parse(o)
-        } catch (e) {
-            (t = {}).data = o
-        } else "object" == typeof o ? t = o : (t = {}).data = o;
-        (Array.isArray(t) || "string" == typeof t) && (t = {
-            data: t,
-            code: 0
-        }), t.code ? t.code = 0 : (t.status, t.status = 0), t.__sekiro_seq__ = e;
-        var n = JSON.stringify(t);
-        console.log("response :" + n), this.socket.send(n)
-    }, SekiroClientTest.prototype.sendFailed = function (e, o) {
-        "string" != typeof o && (o = JSON.stringify(o));
-        var t = {};
-        t.message = o, t.status = -1, t.__sekiro_seq__ = e;
-        var n = JSON.stringify(t);
-        console.log("sekiro: response :" + n), this.socket.send(n)
-    }, SekiroClientTest.prototype.registerAction = function (e, o) {
-        if ("string" != typeof e) throw new Error("an action must be string");
-        if ("function" != typeof o) throw new Error("a handler must be function");
-        return console.log("sekiro: register action: " + e), this.handlers[e] = o, this
-    };
-})();
